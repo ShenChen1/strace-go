7af97e691e335bc00c2a732c1ba3d5f018404425
Peter Shen <peterchenshen@gmail.com>
2026-06-02 04:52:25 +0000

    Fix prctl arity handling to match upstream strace

--- pkg/handler/prctl.go
+++ pkg/handler/prctl.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/binary"
 	"fmt"
 
 	"strace-go/pkg/meta"
@@ -18,33 +19,76 @@ func (h *PrctlHandler) Handle(ctx *Context) Result {
 	option := int32(ctx.Args[0])
 	res.ArgParts = append(res.ArgParts, meta.DecodeFlags(uint64(option), "prctl_options"))
 
-	// Most prctl options use fewer than 5 arguments.
-	// We'll decode based on option.
-	
 	switch option {
 	case 15: // PR_SET_NAME
 		res.ArgParts = append(res.ArgParts, ctx.Decoder.DecodeString(ctx.Pid, ctx.Args[1], ctx.StrArgBuf[0:16], ctx.ArgProbeRet(1), "prctl", ctx.Opts.StringLimit))
+		return res
 	case 16: // PR_GET_NAME
 		if ctx.Ret >= 0 {
 			res.ArgParts = append(res.ArgParts, ctx.Decoder.DecodeString(ctx.Pid, ctx.Args[1], ctx.StrArgBuf[BpfExitArgOffset:1040], ctx.ProbeRetExit, "prctl", ctx.Opts.StringLimit))
 		} else {
-			res.ArgParts = append(res.ArgParts, fmt.Sprintf("%#x", ctx.Args[1]))
+			res.ArgParts = append(res.ArgParts, formatPtrFallback(ctx.Args[1]))
 		}
-	case 1, 2, 3, 4, 11, 12, 13, 14, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 35, 36, 41, 42:
-		// Options that take one integer/pointer argument
-		if option == 1 { // PR_SET_PDEATHSIG
-			res.ArgParts = append(res.ArgParts, meta.DecodeFlags(ctx.Args[1], "signalnames"))
-		} else {
-			res.ArgParts = append(res.ArgParts, fmt.Sprintf("%#x", ctx.Args[1]))
-		}
-	default:
-		// Default to showing all non-zero arguments
-		for i := 1; i < 5; i++ {
-			if ctx.Args[i] != 0 || i == 1 {
-				res.ArgParts = append(res.ArgParts, fmt.Sprintf("%#x", ctx.Args[i]))
+		return res
+	case 1: // PR_GET_PDEATHSIG
+		if ctx.Ret >= 0 && ctx.Args[1] != 0 {
+			data, ok := ctx.FetchStructDataExact(ctx.Args[1], 4, true, nil)
+			if ok {
+				res.ArgParts = append(res.ArgParts, fmt.Sprintf("[%s]", meta.DecodeFlags(uint64(binary.LittleEndian.Uint32(data)), "signalnames")))
+			} else {
+				res.ArgParts = append(res.ArgParts, formatPtrFallback(ctx.Args[1]))
 			}
+		} else {
+			res.ArgParts = append(res.ArgParts, formatPtrFallback(ctx.Args[1]))
 		}
+		return res
+	case 2: // PR_SET_PDEATHSIG
+		res.ArgParts = append(res.ArgParts, meta.DecodeFlags(ctx.Args[1], "signalnames"))
+		return res
+	case 9, 11, 19, 37, 5, 25: // PR_GET_FPEMU, PR_GET_FPEXC, PR_GET_ENDIAN, PR_GET_CHILD_SUBREAPER, PR_GET_UNALIGN, PR_GET_TSC
+		// These return a uint/int in the pointer provided in arg[1]
+		if ctx.Ret >= 0 && ctx.Args[1] != 0 {
+			data, ok := ctx.FetchStructDataExact(ctx.Args[1], 4, true, nil)
+			if ok {
+				res.ArgParts = append(res.ArgParts, fmt.Sprintf("[%d]", int32(binary.LittleEndian.Uint32(data))))
+			} else {
+				res.ArgParts = append(res.ArgParts, formatPtrFallback(ctx.Args[1]))
+			}
+		} else {
+			res.ArgParts = append(res.ArgParts, formatPtrFallback(ctx.Args[1]))
+		}
+		return res
+	case 3, 7, 13, 21, 30, 31, 32, 34, 39, 40, 42, 46, 51, 52, 56, 58, 61, 64, 66, 68, 70, 80:
+		// 0 args
+		return res
+	case 4, 6, 8, 10, 12, 14, 20, 22, 23, 24, 26, 27, 28, 35, 36, 38, 41, 45, 50, 53, 55, 57, 59, 63, 65, 67, 69, 75, 81:
+		// 1 arg
+		res.ArgParts = append(res.ArgParts, formatPtrFallback(ctx.Args[1]))
+		return res
+	case 33: // PR_MCE_KILL
+		res.ArgParts = append(res.ArgParts, formatPtrFallback(ctx.Args[1]))
+		if ctx.Args[1] == 1 { // PR_MCE_KILL_SET
+			res.ArgParts = append(res.ArgParts, fmt.Sprintf("%#x", ctx.Args[2]))
+		}
+		return res
 	}
 
+	// Default to showing all non-zero arguments
+	for i := 1; i < 5; i++ {
+		if ctx.Args[i] != 0 {
+			res.ArgParts = append(res.ArgParts, fmt.Sprintf("%#x", ctx.Args[i]))
+		} else if i == 1 {
+			// upstream strace usually shows arg1 if we don't know the arity, let's keep it if nothing else
+			res.ArgParts = append(res.ArgParts, "0")
+		}
+	}
 	return res
+}
+
+
+func formatPtrFallback(val uint64) string {
+	if val == 0 {
+		return "NULL"
+	}
+	return fmt.Sprintf("%#x", val)
 }
