f1f0c6e8c8df305195a9fb0d161b4eef967de21b
Peter Shen <peterchenshen@gmail.com>
2026-06-30 14:21:47 +0000

    refactor(event): split basic payload section helpers

--- cmd/strace-go/event_payload_sections.go
+++ cmd/strace-go/event_payload_sections.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/base64"
 
 	"strace-go/pkg/handler"
@@ -177,108 +176,6 @@ func structuredPayloadSectionsForEvent(eventRaw *bpfEvent, scName string) ([]han
 	default:
 		return nil, false
 	}
-}
-
-func prlimitPayloadSectionsForEvent(eventRaw *bpfEvent) []handler.PayloadSection {
-	sections := enterStructPayloadSection(eventRaw, 2, handler.BpfEnterArgOffset, rlimitPayloadStructSize)
-	if isExitEvent(eventRaw) && eventRaw.Ret >= 0 {
-		sections = append(sections, exitStructPayloadSection(eventRaw, 3, rlimitPayloadStructSize)...)
-	}
-	return sections
-}
-
-func robustListPayloadSectionsForEvent(eventRaw *bpfEvent) []handler.PayloadSection {
-	if !isExitEvent(eventRaw) || eventRaw.Ret < 0 {
-		return nil
-	}
-	sections := exitStructPayloadSectionAt(eventRaw, 1, handler.BpfExitArgOffset, robustListPayloadWordSize)
-	return append(sections, exitStructPayloadSectionAt(eventRaw, 2, handler.BpfExitArgOffset+16, robustListPayloadWordSize)...)
-}
-
-func waitidPayloadSectionsForEvent(eventRaw *bpfEvent) []handler.PayloadSection {
-	if !isExitEvent(eventRaw) || eventRaw.Ret < 0 {
-		return nil
-	}
-	sections := exitStructPayloadSectionAt(eventRaw, 2, handler.BpfExitArgOffset, waitidSiginfoPayloadSize)
-	return append(sections, exitStructPayloadSectionAt(eventRaw, 4, handler.BpfExitArgOffset+136, waitidRusagePayloadSize)...)
-}
-
-func sendfilePayloadSectionsForEvent(eventRaw *bpfEvent) []handler.PayloadSection {
-	sections := enterStructPayloadSection(eventRaw, 2, handler.BpfMiscArgOffset, offsetPointerPayloadSize)
-	if isExitEvent(eventRaw) && eventRaw.Ret >= 0 {
-		sections = append(sections, exitStructPayloadSection(eventRaw, 2, offsetPointerPayloadSize)...)
-	}
-	return sections
-}
-
-func copyFileRangePayloadSectionsForEvent(eventRaw *bpfEvent) []handler.PayloadSection {
-	sections := enterStructPayloadSection(eventRaw, 1, handler.BpfMiscArgOffset, offsetPointerPayloadSize)
-	return append(sections, enterStructPayloadSection(eventRaw, 3, handler.BpfMiscArgOffset+8, offsetPointerPayloadSize)...)
-}
-
-func stringPayloadSectionFromWindow(eventRaw *bpfEvent, argIndex int) []handler.PayloadSection {
-	data, ok := eventPayloadWindow(eventRaw, 0, 4097)
-	if !ok {
-		return nil
-	}
-	if nul := bytes.IndexByte(data, 0); nul >= 0 {
-		data = data[:nul+1]
-	}
-	section := newPayloadSection(eventRaw, payloadWindowSpec{
-		kind:      handler.PayloadKindString,
-		direction: handler.PayloadDirectionIn,
-		argIndex:  argIndex,
-		userLen:   uint32(len(data)),
-		probeRet:  getArgProbeStatus(eventRaw.ProbeRetEnter, argIndex),
-	}, data)
-	return []handler.PayloadSection{section}
-}
-
-func exitBytesPayloadSectionFromRet(eventRaw *bpfEvent, argIndex int) []handler.PayloadSection {
-	if !isExitEvent(eventRaw) || eventRaw.Ret <= 0 {
-		return nil
-	}
-	userLen := uint32Clamped(uint64(eventRaw.Ret))
-	return payloadSectionFromWindowSpec(eventRaw, payloadWindowSpec{
-		kind:      handler.PayloadKindBytes,
-		direction: handler.PayloadDirectionOut,
-		argIndex:  argIndex,
-		offset:    handler.BpfExitArgOffset,
-		userLen:   userLen,
-		maxLen:    userLen,
-		probeRet:  eventRaw.ProbeRetExit,
-	})
-}
-
-func exitStructPayloadSection(eventRaw *bpfEvent, argIndex int, size uint32) []handler.PayloadSection {
-	return exitStructPayloadSectionAt(eventRaw, argIndex, handler.BpfExitArgOffset, size)
-}
-
-func exitStructPayloadSectionAt(eventRaw *bpfEvent, argIndex int, offset int, size uint32) []handler.PayloadSection {
-	if !isExitEvent(eventRaw) || eventRaw.Ret < 0 {
-		return nil
-	}
-	return payloadSectionFromWindowSpec(eventRaw, payloadWindowSpec{
-		kind:      handler.PayloadKindStruct,
-		direction: handler.PayloadDirectionOut,
-		argIndex:  argIndex,
-		offset:    offset,
-		userLen:   size,
-		maxLen:    size,
-		probeRet:  eventRaw.ProbeRetExit,
-	})
-}
-
-func enterStructPayloadSection(eventRaw *bpfEvent, argIndex int, offset int, size uint32) []handler.PayloadSection {
-	return payloadSectionFromWindowSpec(eventRaw, payloadWindowSpec{
-		kind:      handler.PayloadKindStruct,
-		direction: handler.PayloadDirectionIn,
-		argIndex:  argIndex,
-		offset:    offset,
-		userLen:   size,
-		maxLen:    size,
-		probeRet:  getArgProbeStatus(eventRaw.ProbeRetEnter, argIndex),
-	})
 }
 
 func pollPayloadSectionsForEvent(eventRaw *bpfEvent, includeTimeout bool) []handler.PayloadSection {
