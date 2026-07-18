73728b6c2ec5890e15ec6dcc3fec0b227919487b
Peter Shen <peterchenshen@gmail.com>
2026-07-18 09:26:29 +0000

    refactor(event): drop fixed-window record decoder

--- cmd/strace-go/trace_record_decoder.go
+++ cmd/strace-go/trace_record_decoder.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"unsafe"
-
-	"github.com/cilium/ebpf/ringbuf"
-)
+import "github.com/cilium/ebpf/ringbuf"
 
 type traceRecordDecoder interface {
 	Decode(rec *ringbuf.Record) (traceEventEnvelope, bool)
@@ -25,23 +21,4 @@ func (d traceRingbufRecordDecoder) Decode(rec *ringbuf.Record) (traceEventEnvelo
 		return traceEventEnvelope{}, false
 	}
 	return decodeTraceEventV2Envelope(rec.RawSample)
-}
-
-func decodeFixedWindowTraceEventEnvelope(rawSample []byte) (traceEventEnvelope, bool) {
-	ev, ok := decodeFixedWindowBPFEvent(rawSample)
-	if !ok {
-		return traceEventEnvelope{}, false
-	}
-	return newTraceEventEnvelopeFromBPF(&ev), true
-}
-
-func decodeFixedWindowBPFEvent(rawSample []byte) (bpfEvent, bool) {
-	var ev bpfEvent
-	minSize := int(unsafe.Offsetof(ev.StrArg))
-	if len(rawSample) < minSize {
-		return ev, false
-	}
-	eventBytes := unsafe.Slice((*byte)(unsafe.Pointer(&ev)), int(unsafe.Sizeof(ev)))
-	copy(eventBytes, rawSample)
-	return ev, true
 }
