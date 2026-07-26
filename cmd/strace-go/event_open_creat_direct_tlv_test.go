57aa47c9fe8ad1b0eb5c3f45594b784465edf93d
Peter Shen <peterchenshen@gmail.com>
2026-07-26 09:13:47 +0000

    test(event): drive open creat TLV tests through event v2

--- cmd/strace-go/event_open_creat_direct_tlv_test.go
+++ cmd/strace-go/event_open_creat_direct_tlv_test.go
@@ -35,12 +35,11 @@ func TestSyscallEventContextMergesOpenCreatDirectTLVPath(t *testing.T) {
 				userLen: uint32(len(pathData)),
 				data:    pathData,
 			})
-			enterRaw := miscStructTLVEvent(t, tt.name, bpfEventTypeEnter, tt.args, 0, enterPayload)
-			enterRaw.EventFlags |= bpfEventFlagGenericEnter
-			session.traceState().handleEnvelope(newTraceEventEnvelopeFromBPF(enterRaw))
+			enterEnvelope := testTLVSyscallEnvelope(t, tt.name, bpfEventTypeEnter, tt.args, 0, enterPayload)
+			session.traceState().handleEnvelope(enterEnvelope)
 
-			exitRaw := miscStructTLVEvent(t, tt.name, bpfEventTypeExit, tt.args, 7, nil)
-			exitUpdate := session.traceState().handleEnvelope(newTraceEventEnvelopeFromBPF(exitRaw))
+			exitEnvelope := testTLVSyscallEnvelope(t, tt.name, bpfEventTypeExit, tt.args, 7, nil)
+			exitUpdate := session.traceState().handleEnvelope(exitEnvelope)
 			ev := newSyscallEventContextFromView(
 				session,
 				exitUpdate.syscallView,
